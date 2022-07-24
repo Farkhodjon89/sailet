0a7da62a27e2ac26d077a148df17a31e028f8214
farhod <sagatov-99@mail.ru>
2022-07-24 12:14:44 +0500

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,14 +3,12 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
   return (
       <Html>
-        <Head>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
-        </Head>
+        <Head/>
         <body>
         <Main/>
         <div id='portal'/>
         <NextScript/>
         </body>
       </Html>
-)
+  )
 }
